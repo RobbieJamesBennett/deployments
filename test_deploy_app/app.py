4f35b04ca84b9b28b8e62031b57c3907c0e459a3
robbiejamesbennett <robbiejamesbennett@gmail.com>
2023-07-05 13:15:17 +0100

    updates for sql server addition

--- test_deploy_app/app.py
+++ test_deploy_app/app.py
@@ -1,25 +1,50 @@
-from flask import Flask, jsonify
-from sqlalchemy import create_engine, Table, MetaData
-
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+from sqlalchemy import create_engine
+import pandas as pd
+import secrets
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secrets.token_hex(16)
+# SQL Login form
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
 
-CONN_STRING = 'mssql+pyodbc://sqlserver:Ibottest1@db-1.database.windows.net/db_1?driver=ODBC+Driver+17+for+SQL+Server'
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        # Save the credentials in Flask's session
+        session['username'] = username
+        session['password'] = password
+        # Redirect the user to the page where you want to show the SQL data
+        return redirect(url_for('show_data'))
+    return render_template('login.html', title='Sign In', form=form)
 
-@app.route('/')
-def hello_world():
-    engine = create_engine(CONN_STRING)
+@app.route('/data')
+def show_data():
+    # Get the credentials from Flask's session
+    username = session.get('username')
+    password = session.get('password')
 
-    metadata = MetaData()
-    test_table = Table('test_table', metadata, autoload_with=engine)
+    if not username or not password:
+        # If the credentials are not present in the session, redirect the user to the login page
+        return redirect(url_for('login'))
 
-    with engine.connect() as connection:
-        result = connection.execute(test_table.select())
-        rows = [dict(row) for row in result]
-
-    return jsonify(rows)  # convert the result to a list of dicts
+    # Use the credentials to create a SQLAlchemy engine
+    conn_str = f"mssql+pyodbc://{username}:{password}@server_name/database_name?driver=SQL Server"
+    engine = create_engine(conn_str)
+    df = pd.read_sql("SELECT * FROM test_table", engine)
+    return render_template('data.html', data=df.to_html())
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
 
