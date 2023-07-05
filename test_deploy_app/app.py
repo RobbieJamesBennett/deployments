6be70a549b2ccd8c487652c872d9f884448c78a2
robbiejamesbennett <robbiejamesbennett@gmail.com>
2023-07-05 12:17:01 +0100

    testing sql connection

--- test_deploy_app/app.py
+++ test_deploy_app/app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
+from sqlalchemy import create_engine, Table, MetaData
+
 app = Flask(__name__)
+
+CONN_STRING = 'mssql+pyodbc://sqlserver:Ibottest1@db-1.database.windows.net/db_1?driver=ODBC+Driver+17+for+SQL+Server'
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    engine = create_engine(CONN_STRING)
+
+    metadata = MetaData()
+    test_table = Table('test_table', metadata, autoload_with=engine)
+
+    with engine.connect() as connection:
+        result = connection.execute(test_table.select())
+        rows = [dict(row) for row in result]
+
+    return jsonify(rows)  # convert the result to a list of dicts
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
+
+
+
+
